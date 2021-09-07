ae73b2aa54953e9ae37aadc36ca5dc39304f6c18
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-09-07 13:45:10 +0700

    Add Play page skeleton

--- pages/play.tsx
+++ pages/play.tsx
@@ -8,7 +8,16 @@ const Play: NextPage = () => {
       <Head>
         <title>koddl.io - Play</title>
       </Head>
-      <div>Play Page</div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="grid grid-cols-3">
+          <div className="border-2 border-red-500">Counter, Round</div>
+          <div className="border-2 border-red-500">Hidden Word</div>
+          <div className="border-2 border-red-500">Language Chooser/Your Word</div>
+          <div className="border-2 border-red-500">Players Sidebar</div>
+          <div className="border-2 border-red-500">Code Editor</div>
+          <div className="border-2 border-red-500">Chat Sidebar</div>
+        </div>
+      </div>
     </>
   )
 }
