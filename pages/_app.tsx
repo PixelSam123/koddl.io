a8d58ed478d9067a202034e186d9b5e22219ce9d
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-09-07 13:45:00 +0700

    Update _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-yellow-100">
+    <div className="bg-yellow-100 min-h-screen">
       <Component {...pageProps} />
     </div>
   )
