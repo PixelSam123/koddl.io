d351bfb466fe8795c7471d7c7e3457c2e3701f20
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-09-07 14:47:41 +0700

    Progress on play page

--- pages/play.tsx
+++ pages/play.tsx
@@ -1,22 +1,34 @@
 import type { NextPage } from 'next'
 
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 const Play: NextPage = () => {
+  const [time, setTime] = useState(65)
+  const [round, setRound] = useState(1)
+  const [maxRound, setMaxRound] = useState(3)
+
+  const [hiddenWord, setHiddenWord] = useState('________')
+
   return (
     <>
       <Head>
         <title>koddl.io - Play</title>
       </Head>
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="grid grid-cols-3">
-          <div className="border-2 border-red-500">Counter, Round</div>
-          <div className="border-2 border-red-500">Hidden Word</div>
-          <div className="border-2 border-red-500">Language Chooser/Your Word</div>
-          <div className="border-2 border-red-500">Players Sidebar</div>
-          <div className="border-2 border-red-500">Code Editor</div>
-          <div className="border-2 border-red-500">Chat Sidebar</div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 grid font-mono">
+        <div className="col-span-3 flex items-center gap-x-2 bg-yellow-50">
+          <div className="w-max flex items-center">
+            <p className="w-10 text-center text-3xl">{time}</p>
+            <p>
+              Round {round} of {maxRound}
+            </p>
+          </div>
+          <p className="w-max text-lg tracking-widest">{hiddenWord}</p>
+          <div className="w-max mr-2">Language Chooser/Your Word</div>
         </div>
+        <div className="border-2 border-red-500">Players Sidebar</div>
+        <div className="border-2 border-red-500">Code Editor</div>
+        <div className="border-2 border-red-500">Chat Sidebar</div>
       </div>
     </>
   )
