d66978020c828e77e3ad94cff66e3c59d833777e
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-09-08 15:05:37 +0700

    Make default value of SocketContext null

--- context/SocketContext.ts
+++ context/SocketContext.ts
@@ -1,8 +1,10 @@
+import type { Socket } from 'socket.io-client'
+
 import { createContext } from 'react'
 import { io } from 'socket.io-client'
 
 const socket = io()
-const SocketContext = createContext(socket)
+const SocketContext = createContext<null | Socket>(null)
 
 export { socket }
 export default SocketContext
