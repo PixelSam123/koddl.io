328cd95869675f661f50be00ca623bbf1f1c81a9
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-09-08 15:08:10 +0700

    Revert SocketContext default value to socket reference

--- context/SocketContext.ts
+++ context/SocketContext.ts
@@ -1,10 +1,8 @@
-import type { Socket } from 'socket.io-client'
-
 import { createContext } from 'react'
 import { io } from 'socket.io-client'
 
 const socket = io()
-const SocketContext = createContext<null | Socket>(null)
+const SocketContext = createContext(socket)
 
 export { socket }
 export default SocketContext
