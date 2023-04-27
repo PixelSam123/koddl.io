f6a45ddec4b772a3a80ff07acd697d9c659d8a0e
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-04-27 22:30:59 +0700

    update tailwindcss config to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
+  content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/layouts/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
